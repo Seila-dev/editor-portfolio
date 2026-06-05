46d076530df5565d95d6a9282a4556e1a8843229
unknown <erickoliveira3975@gmail.com>
2026-06-05 13:10:25 -0300

    update

--- src/components/ProjectsCarousel/index.tsx
+++ src/components/ProjectsCarousel/index.tsx
@@ -2,8 +2,9 @@
 import styled from "styled-components";
 
 const videos = [
+  { id: "vdpA_fmeGL8", title: "Client Long-form Intro" },
   // https://youtu.be/cSo4_s9w7pw?si=8D28RWVNeFkN_Ip8
-  { id: "pmbxZXFSWPU", title: "Client Long-form Intro" },
+  // { id: "pmbxZXFSWPU", title: "Client Long-form Intro" },
   { id: "ZkYtbBloA8w", title: "ItsPoachie video" },
   { id: "5hIW38ni1Ic", title: "Beating Pokémon Diamond with the Hardest to Catch Team!" },
   { id: "9LYEmgG8WoM", title: "ItsRJ Trial" },
